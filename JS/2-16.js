2451ddc676b8132e04575129159a3fd4704156fc
Alexvav <alexvav1984@gmail.com>
2021-11-04 23:53:21 +0300

    All

---
+++ JS/2-16.js
@@ -0,0 +1,26 @@
+"use strict";
+
+function showFirstMessage(text){
+    console.log(text);
+
+}
+
+showFirstMessage("Hello World");
+
+
+function calc(a, b){
+    return (a+b);
+}
+
+console.log(calc(4, 5));
+
+function ret(){
+    let num = 50;
+
+    //
+
+    return num;
+}
+
+const anitherNum = ret();
+console.log(anitherNum);
---
+++ JS/2-17.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const str = "teSt";
+const arr = [1, 2, 4];
+
+// console.log(str.length);
+// console.log(str[2]);
+// console.log(str.toUpperCase());
+
+console.log(str.toLowerCase());
+console.log(str);
+
+const fruit = "Some fruit";
+
+console.log
